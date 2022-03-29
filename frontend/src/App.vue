18eb2fa156148f446e66a82141c135f81e2410e8
nuruhee <nuruhee@gmail.com>
2022-03-29 11:16:20 +0900

    추가 파일

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <button @click="test1">Get결과출력</button><br>
+  <router-view></router-view>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-  components: {
-  },
-  methods: {
-    test1() {
-      console.log("111")
-      console.log("222")
-      const formData = new FormData()
-      formData.append('email', '111')
-      this.axios.get('/api/test', formData).then(res => {
-        console.log(res.data)
-      })
-    }
-  }
-}
+export default {};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
