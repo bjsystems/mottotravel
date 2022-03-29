75e11a3cf49314e160202fcecd7c27729e5b930e
nuruhee <nuruhee@gmail.com>
2022-03-29 17:58:32 +0900

    여행 코스 페이지 추가

---
+++ frontend/src/pages/TravelCourse.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <md-card v-for="(item, index) in videoList" :key="index.video_id">
+      <md-card-header style="margin-top: 0px; margin-left: 0px; margin-right: 0px;">
+        <md-avatar>
+          <img :src=item.profile_image_url alt="Avatar">
+        </md-avatar>
+        <div class="md-title" style="margin-top: 5px;">{{ item.create_name }}</div>
+        <div class="md-subhead" style="white-space : nowrap; text-overflow: ellipsis; overflow : hidden;">{{ item.video_title }}</div>
+      </md-card-header>
+      <md-card-media>
+        <img :src=item.thumbnail_url alt="People">
+      </md-card-media>
+      <md-card-actions md-alignment="left">
+        업로드일 : 2022년 03월 29일 · 댓글 : 3000개 <br>
+        조회수 21900회 · 좋아요 1000개
+      </md-card-actions>
+      <div class="card-expansion">
+        <md-card>
+          <md-card-expand>
+            <md-card-actions md-alignment="space-between">
+              <md-card-expand-trigger>
+                <md-button class="md-icon-button">
+                  <md-icon>keyboard_arrow_down</md-icon>
+                </md-button>
+              </md-card-expand-trigger>
+            </md-card-actions>
+            <md-card-expand-content>
+              <md-card-content>
+                동영상 설명
+              </md-card-content>
+            </md-card-expand-content>
+          </md-card-expand>
+        </md-card>
+      </div>
+      <md-card-actions>
+        <md-button class="md-icon-button">
+          <md-icon>thumb_up</md-icon>
+        </md-button>
+        <md-button class="md-icon-button" md-alignment="right">
+          <md-icon>favorite</md-icon>
+        </md-button>
+        <md-button class="md-icon-button">
+          <md-icon>share</md-icon>
+        </md-button>
+      </md-card-actions>
+    </md-card>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TravelCourse"
+}
+</script>
+
+<style scoped>
+
+</style>
